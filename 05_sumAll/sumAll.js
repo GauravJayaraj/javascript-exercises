8884afa271e84074b470b49a687fc49f60f28022
Gaurav J <gauravjayaraj@gmail.com>
2021-12-23 08:38:05 -0500

    07 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(start , end) {
+    let sum=0;
+    console.log(typeof(start),typeof(end), typeof(Number()));
 
+    if(typeof(start)!=typeof(Number()) || typeof(end)!=typeof(Number()))
+        return "ERROR";
+    
+    if(start<0 || end<0)
+        return "ERROR";
+
+    if(start>end)
+    {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+
+    for(let i=start; i<=end; i++){
+        sum+=i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = (farenheit - 32)*(5/9);
+  
+  console.log(celcius);
+  // round to 1 decimal
+  celcius = Math.round(celcius*10)/10;    // better this way
+                                      // than round(x,1);
+  return celcius;
 };
 
-const ctof = function() {
+const ctof = function(celcius) {
+  let farenheit = ((9/5)*celcius)+32;
 
+  // round to 1 decimal
+  farenheit = Math.round(farenheit*10)/10;
+
+  return farenheit;
 };
 
 // Do not edit below this line
